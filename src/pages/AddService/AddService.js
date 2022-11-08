f9a968a40a8d2f5b72858d57c3ab8821fb35c188
Shahin Alam <shahinzzz1949@gmail.com>
2022-11-08 20:40:21 +0600

    client site initilize

---
+++ src/pages/AddService/AddService.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddService = () => {
+    return (
+        <div>
+            <h1>Iam add services pages</h1>
+        </div>
+    );
+};
+
+export default AddService;
