e867ca67c145be2a7b896b52d6c5840160e03281
Shahin Alam <shahinzzz1949@gmail.com>
2022-11-09 22:38:03 +0600

    add update section dynamic

--- src/pages/Reviews/Reviews.js
+++ src/pages/Reviews/Reviews.js
@@ -2,13 +2,22 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Menubar from '../../components/Navbar/Menubar';
 import { AuthContextInfo } from '../../cotext/Authcontext';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 const Reviews = () => {
     const [review, setReview] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [ratting, setRatting] = useState();
+    const [text, setText] = useState();
+    const [id, setId] = useState();
     const {user} = useContext(AuthContextInfo);
     const email = user.email;
     console.log(review);
+
+    // modals
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
 
     useEffect(() => {
         const url = `http://localhost:5000/reviews-by-email?email=${email}`;
@@ -26,6 +35,7 @@ const Reviews = () => {
     console.log(review);
 
 
+    // delete review
     const deleteReview = (id) => {
         console.log(id);
         const reviewId = id;
@@ -47,6 +57,44 @@ const Reviews = () => {
         })
         .catch(error => console.log(error));
     }
+
+    // Update review
+    const handleShow = (data) => {
+        console.log(data);
+        setRatting(data.ratings);
+        setText(data.revText);
+        setId(data._id);
+        setShow(true);
+    }
+    // Update data
+    const upadteData = () => {
+        console.log(ratting, text, id);
+        const updatedRatting = parseFloat(ratting).toFixed(1);
+        const updatedText = text;
+
+        if(updatedRatting !== '' && updatedText !== ''){
+            fetch('http://localhost:5000/update-review', {
+                method : 'PUT',
+                headers : {
+                    'content-type' : 'application/json'
+                },
+                body : JSON.stringify({updatedRatting, updatedText, id})
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if(data.acknowledged && data.modifiedCount > 0){
+                    setShow(false);
+                }else{
+                    alert('interal error! please again try')
+                }
+            })
+            .catch(error => console.log(error));
+        }else{
+            alert('all filled required');
+        }
+    }
+
 
     return (
         <div>
@@ -74,6 +122,7 @@ const Reviews = () => {
                                         key={r._id}
                                         eachReview = {r}
                                         deleteReview ={deleteReview}
+                                        handleShow = {handleShow}
                                         ></ReviewLoadByEmail>
                                     )
                                     })
@@ -84,14 +133,43 @@ const Reviews = () => {
                 }
                 
             </div>
+
+
+            {/* review update modal */}
+
+
+            <>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Update review</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <label className='mt-3 mb-2 ' htmlFor="">Rattings</label>
+            <input onBlur={(e) => setRatting(e.target.value)} type="number" className='form-control' min='1' required max='5' step='0.01' defaultValue={ratting} />
+            <label className='mt-3 mb-2' htmlFor="">Your review text</label>
+            <textarea onBlur={(e) => setText(e.target.value)} defaultValue={text} id='review-text' name="" className='w-100 form-control' rows="3"></textarea>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Dismiss
+          </Button>
+          <Button onClick={upadteData} variant="primary">Update</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
         </div>
     );
 };
 
 // all reviews load
-const ReviewLoadByEmail = ({eachReview , deleteReview}) => {
+const ReviewLoadByEmail = ({eachReview , deleteReview, handleShow}) => {
 
-    console.log(eachReview);
     return (
             <div className='alert alert-success px-2 py-2 w-100 my-3 d-flex flex-wrap justify-content-between'>
                     <div>
@@ -108,7 +186,7 @@ const ReviewLoadByEmail = ({eachReview , deleteReview}) => {
                     </div>
                     <div className='d-flex align-items-center'>
                         <Link to={`/services/details/${eachReview.foodId}`}><button className='btn btn-sm btn-light text-primary me-3'>Visit service</button></Link>
-                        <button className='btn btn-primary btn-sm me-3'>Edit</button>
+                        <button onClick={() => handleShow(eachReview)} className='btn btn-primary btn-sm me-3'>Edit</button>
                         <button onClick={() => deleteReview(eachReview._id)} className='btn btn-danger btn-sm me-3'>Delete</button>
                     </div>
             </div>
