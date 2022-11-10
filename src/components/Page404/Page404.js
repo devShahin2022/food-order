0781446b306c1538fb4a4ae6502d309e12589b79
Shahin Alam <shahinzzz1949@gmail.com>
2022-11-10 16:03:12 +0600

    link add in 404 page

--- src/components/Page404/Page404.js
+++ src/components/Page404/Page404.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Page404 = () => {
     return (
         <div className='container'>
             <img className='w-100 img-fluid' src="https://img.freepik.com/free-vector/error-404-concept-landing-page_52683-21357.jpg?size=626&ext=jpg&ga=GA1.2.1380690759.1667906269&semt=sph" alt="" />
             <div className='d-flex justify-content-center my-5 mb-5'>
-                <button className='btn btn-primary py-3 px-5'>Go to home page</button>
+                <Link to='/'><button className='btn btn-primary py-3 px-5'>Go to home page</button></Link>
             </div>
         </div>
     );
